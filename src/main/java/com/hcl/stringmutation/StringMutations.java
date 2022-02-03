b983192a1ff42e5502ac7f7f25091cedc37b8bd8
Patrick Fuller <patrickbfuller@gmail.com>
2022-02-03 12:00:49 -0500

    Format whitespace

--- src/main/java/com/hcl/stringmutation/StringMutations.java
+++ src/main/java/com/hcl/stringmutation/StringMutations.java
@@ -4,12 +4,11 @@ import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class StringMutations {
-	
+
 	public static void main(String[] args) {
-		
-		
+
 		Scanner scanner = new Scanner(System.in);
-		
+
 		System.out.println("Please enter first name:");
 		String first = scanner.next();
 		System.out.println("Please enter middle name:");
@@ -20,17 +19,17 @@ public class StringMutations {
 		String date = scanner.next();
 		System.out.println("Please enter favorite color:");
 		String color = scanner.next();
-		
+
 		scanner.close();
-		
+
 		String fullName = String.join("_", first, middle, last);
-		
+
 		// Part 1
 		// f_m_l -> f:m:l
 		System.out.println("Full name: " + fullName);
-		
-		StringBuffer sb1 = new StringBuffer(); 
-		
+
+		StringBuffer sb1 = new StringBuffer();
+
 		StringTokenizer tokenizer1 = new StringTokenizer(fullName, "_");
 		while (tokenizer1.hasMoreTokens()) {
 			sb1.append(tokenizer1.nextToken());
@@ -38,13 +37,13 @@ public class StringMutations {
 				sb1.append(":");
 			}
 		}
-		
+
 		System.out.println("Full name with colons: " + sb1.toString());
-		
+
 		// Part 2
-		// f_m_l -> f m l		
-		StringBuffer sb2 = new StringBuffer(); 
-		
+		// f_m_l -> f m l
+		StringBuffer sb2 = new StringBuffer();
+
 		StringTokenizer tokenizer2 = new StringTokenizer(fullName, "_");
 		while (tokenizer2.hasMoreTokens()) {
 			sb2.append(tokenizer2.nextToken());
@@ -52,10 +51,9 @@ public class StringMutations {
 				sb2.append(" ");
 			}
 		}
-		
+
 		System.out.println("Full name with colons: " + sb2.toString());
-		
-		
+
 		// Part 3
 		// dd/mm/yyyy
 		StringTokenizer tokenizer3 = new StringTokenizer(date, "/");
@@ -65,9 +63,7 @@ public class StringMutations {
 		System.out.println("Days: " + days);
 		System.out.println("Months: " + months);
 		System.out.println("Years: " + years);
-		
 
-		
 		// Part 4
 		// f_m_l -> f m l d1 d2
 		sb2.append(" ");
